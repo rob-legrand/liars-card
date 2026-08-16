bac66695f1e54486dd0eaa220dcf18c851b7ba40
Rob LeGrand <roblegrand98@gmail.com>
2026-08-16 14:22:04 -0500

    Allow keyboard input

--- script.js
+++ script.js
@@ -2,20 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
    'use strict';
-   var chooseOpponentClaim, game, historyCanvas, historyContext, makePlayerClaim, playerClaimCards, playerHandCards, opponentClaimCards, opponentHandCards, saveGame, startNewHand, updateGameboard;
-
-   if (!localStorage) {
-      window.alert('Your browser does not seem to support localStorage correctly.\nWe recommend a recent version of a standards-compliant browser such as Opera, Chrome or Firefox.');
-   }
-
-   game = {
-      score: 0,
-      scoreChange: 0,
-      scoreHistory: [],
-      lowestScore: 0,
-      highestScore: 0,
-      numCards: 4
-   };
+   var chooseOpponentClaim, game, historyCanvas, historyContext, playerClaimCards, playerHandCards, opponentClaimCards, opponentHandCards, saveGame, startNewHand, updateGameboard;
 
    saveGame = function () {
       if (!localStorage) {
@@ -125,59 +112,105 @@ document.addEventListener('DOMContentLoaded', function () {
       var allInProbs, claim;
       allInProbs = [0.3, 0.1, 0.9, 1.0];
       claim = 0;
-      while (claim < game.numCards - 1 && Math.random() < allInProbs[card]) {
-         claim += 1;
+      if (card >= 0 && card < game.numCards) {
+         while (claim < game.numCards - 1 && Math.random() < allInProbs[card]) {
+            claim += 1;
+         }
       }
       return claim;
    };
 
-   makePlayerClaim = function (card) {
-      return function () {
-         if (game.hasOwnProperty('playerCard') && !game.hasOwnProperty('playerClaim')) {
-            game.playerClaim = card;
-            game.opponentClaim = chooseOpponentClaim(game.opponentCard);
-            if (game.playerClaim > game.opponentClaim) {
-               game.scoreChange = game.opponentClaim + 1;
-            } else if (game.playerClaim < game.opponentClaim) {
-               game.scoreChange = -game.playerClaim - 1;
-            } else if (game.playerCard > game.opponentCard) {
-               game.scoreChange = game.opponentClaim + 1;
-            } else if (game.playerCard < game.opponentCard) {
-               game.scoreChange = -game.playerClaim - 1;
-            } else {
-               game.scoreChange = 0;
-            }
-            updateGameboard();
-         }
-      };
-   };
-
-   playerHandCards = [];
-   playerClaimCards = [];
-   opponentClaimCards = [];
-   opponentHandCards = [];
-
    (function () {
-      var whichCard;
+      var createPlayerClaimFunc, newGame, playerClaimFuncs, whichCard;
+
+      newGame = {
+         score: 0,
+         scoreChange: 0,
+         scoreHistory: [],
+         lowestScore: 0,
+         highestScore: 0,
+         numCards: 4
+      };
+
+      game = localStorage && localStorage.liarsCardGame ? JSON.parse(localStorage.liarsCardGame) : {};
+      Object.keys(newGame).forEach(function (prop) {
+         if (!game.hasOwnProperty(prop)) {
+            game[prop] = newGame[prop];
+         }
+      });
+
+      createPlayerClaimFunc = function (card) {
+         return function () {
+            if (game.hasOwnProperty('playerCard') && !game.hasOwnProperty('playerClaim')) {
+               game.playerClaim = card;
+               game.opponentClaim = chooseOpponentClaim(game.opponentCard);
+               if (game.playerClaim > game.opponentClaim) {
+                  game.scoreChange = game.opponentClaim + 1;
+               } else if (game.playerClaim < game.opponentClaim) {
+                  game.scoreChange = -game.playerClaim - 1;
+               } else if (game.playerCard > game.opponentCard) {
+                  game.scoreChange = game.opponentClaim + 1;
+               } else if (game.playerCard < game.opponentCard) {
+                  game.scoreChange = -game.playerClaim - 1;
+               } else {
+                  game.scoreChange = 0;
+               }
+               updateGameboard();
+            }
+         };
+      };
+
+      playerHandCards = [];
+      playerClaimCards = [];
+      opponentClaimCards = [];
+      opponentHandCards = [];
+      playerClaimFuncs = [];
+
       for (whichCard = 0; whichCard < game.numCards; whichCard += 1) {
-         playerHandCards[whichCard] = document.getElementById('player-hand-' + (whichCard + 1));
-         playerClaimCards[whichCard] = document.getElementById('player-claim-' + (whichCard + 1));
-         playerClaimCards[whichCard].addEventListener('click', makePlayerClaim(whichCard));
-         opponentClaimCards[whichCard] = document.getElementById('opponent-claim-' + (whichCard + 1));
-         opponentHandCards[whichCard] = document.getElementById('opponent-hand-' + (whichCard + 1));
+         playerHandCards.push(document.getElementById('player-hand-' + (whichCard + 1)));
+         playerClaimCards.push(document.getElementById('player-claim-' + (whichCard + 1)));
+         opponentClaimCards.push(document.getElementById('opponent-claim-' + (whichCard + 1)));
+         opponentHandCards.push(document.getElementById('opponent-hand-' + (whichCard + 1)));
+      }
+      playerClaimCards.forEach(function (element, whichCard) {
+         playerClaimFuncs.push(createPlayerClaimFunc(whichCard));
+         element.addEventListener('click', playerClaimFuncs[whichCard], false);
+         element.addEventListener('mousedown', function (ev) {
+            ev.preventDefault();
+         }, false);
+      });
+
+      document.getElementById('start-next-hand').addEventListener('click', startNewHand, false);
+
+      document.addEventListener('keypress', function (ev) {
+         var card, key, specialStrategies;
+         key = (ev.key || String.fromCharCode(ev.charCode)).toLowerCase();
+         specialStrategies = {
+            a: [3, 3, 3, 3], // always aggressively all-in
+            h: [0, 1, 2, 3], // honest
+            s: [1, 0, 0, 3], // best strategy against default AI ("sneaky")
+            w: [3, 3, 2, 0], // worst strategy against default AI
+            z: [0, 0, 3, 3]  // all-in when likely to win a showdown
+         };
+         if (specialStrategies[key]) {
+            card = specialStrategies[key][game.playerCard];
+         } else {
+            card = key.charCodeAt(0) - '1'.charCodeAt(0);
+         }
+         if (game.hasOwnProperty('opponentClaim')) {
+            startNewHand();
+         } else if (card >= 0 && card < game.numCards) {
+            playerClaimFuncs[card]();
+         }
+      }, false);
+
+      historyCanvas = document.getElementById('score-history');
+      historyContext = historyCanvas && historyCanvas.getContext && historyCanvas.getContext('2d');
+
+      if (!game.hasOwnProperty('playerCard')) { // if starting new history from scratch
+         startNewHand();
+      } else {
+         updateGameboard();
       }
    }());
-
-   document.getElementById('start-next-hand').addEventListener('click', startNewHand);
-
-   historyCanvas = document.getElementById('score-history');
-   historyContext = historyCanvas && historyCanvas.getContext && historyCanvas.getContext('2d');
-
-   if (localStorage && localStorage.liarsCardGame) {
-      game = JSON.parse(localStorage.liarsCardGame);
-      updateGameboard();
-   }
-   if (!game.hasOwnProperty('playerCard')) { // if starting new history from scratch
-      startNewHand();
-   }
-});
+}, false);
